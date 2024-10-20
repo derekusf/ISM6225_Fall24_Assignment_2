d8546fc26bef7ef9dfb00c9b542999b0f0fa88fe
LittelEvil <derektran@outlook.sg>
2024-10-20 18:28:08 -0400

    Implement methods for Assignment 2

--- ISM6225_Spring_2024_Assignment_2/Program.cs
+++ ISM6225_Spring_2024_Assignment_2/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
 
 namespace Assignment_2
 {
@@ -13,7 +17,7 @@ namespace Assignment_2
             IList<int> missingNumbers = FindMissingNumbers(nums1);
             Console.WriteLine(string.Join(",", missingNumbers));
 
-            // Question 2: Sort Array by Parity
+            //Question 2: Sort Array by Parity
             Console.WriteLine("Question 2:");
             int[] nums2 = { 3, 1, 2, 4 };
             int[] sortedArray = SortArrayByParity(nums2);
@@ -63,7 +67,41 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return new List<int>(); // Placeholder
+                List<int> missingItems = new List<int>();
+                // Check the empty list
+                if (nums.Length == 0){
+                    return missingItems;
+                }
+                else{ //For positive cases
+                    Array.Sort(nums);                   //sort the O(nlogn)
+                    int j = 0;                          // Running index to keep track of assessed element
+                    for(int i=1; i <= nums.Length; i++ ){ //traverse to all possible values
+                        if (j >= nums.Length){ //have traversed all elements of nums
+                          missingItems.Add(i);
+                          continue;
+                        }
+                        for (int k = j; k < nums.Length; k++){ //traverse through all elements of nums
+                            if(nums[k] < i){
+                                //If element less than expected value, continue to find next item
+                                //If reach the end of the list, add missing value
+                                if (k == nums.Length-1){
+                                    missingItems.Add(i);
+                                }
+                                j++;
+                                continue;
+                            }
+                            else if (nums[k] == i){// found it
+                                j++;
+                                break;
+                            } 
+                            else if (nums[k] > i){// No value found aka missing
+                                missingItems.Add(i);
+                                break;
+                            } 
+                        }
+                    }
+                    return missingItems;
+                }
             }
             catch (Exception)
             {
@@ -77,7 +115,27 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return new int[0]; // Placeholder
+                int[] output = new int[nums.Length];
+                if (nums.Length == 0){// Check the empty list
+                    return new int[0];
+                }
+                int index = 0; 
+                //Traverse through the nums, get all even integers
+                for (int i = 0; i< nums.Length; i++){
+                    if (nums[i] % 2 == 0){
+                        output[index] = nums[i];
+                        index ++;
+                    }
+                }
+                //Traverse through the nums, get all odd integers
+                for (int i = 0; i< nums.Length; i++){
+                    if (nums[i] % 2 == 1){
+                        output[index] = nums[i];
+                        index ++;
+                    }
+                }
+
+                return output; // Placeholder
             }
             catch (Exception)
             {
@@ -91,7 +149,24 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return new int[0]; // Placeholder
+                int[] output = {-1,-1};
+                if (nums.Length == 0){// Check the empty list
+                    return new int[0];
+                }
+                //Traverse through the nums for the 1st factor
+                for (int i = 0; i< nums.Length; i++){
+                    //Traverse through the nums for the 2nd factor
+                    for (int j = 0; j< nums.Length; j++){
+                        if(i==j) continue; //skip if same element
+                        if(nums[i] + nums[j] == target){ // found the factors
+                            output[0] = i;
+                            output[1] = j;
+                            return output; 
+                        }
+                    }
+                }
+
+                return output; //if not found return {-1,-1}
             }
             catch (Exception)
             {
@@ -105,7 +180,41 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return 0; // Placeholder
+                if (nums.Length == 0){// Check the empty list
+                    return 0;
+                }
+                int maxp = 0;
+                Array.Sort(nums); 
+                //As max production of 3 are always the combination of 3 top positive elements
+                // or 2 smallest negative elements * 1 largest positive element
+                // or 3 top negative elements (if all are negative)
+                // --> Get 2 smallest and 3 largest for assessment. 
+                int[] assess_array; 
+                if(nums.Length >= 5){
+                    assess_array = new int[5];
+                    assess_array[0] = nums[0]; 
+                    assess_array[1] = nums[1]; 
+                    assess_array[2] = nums[nums.Length-3]; 
+                    assess_array[3] = nums[nums.Length-2]; 
+                    assess_array[4] = nums[nums.Length-1]; 
+                }
+                else{
+                    assess_array = nums;
+                }
+                //Traverse through the nums for the 1st factor 
+                for (int i = 0; i< assess_array.Length; i++){ 
+                    //Traverse through the nums for the 2nd factor
+                    for (int j = i+1; j< assess_array.Length; j++){
+                        //Traverse through the nums for the 3rd factor
+                        for (int k = j+1; k <assess_array.Length; k++){
+                            int prod = assess_array[i]*assess_array[j]*assess_array[k];
+                            if (prod > maxp){
+                                maxp = prod;
+                            }
+                        }
+                    }
+                }
+                return maxp; 
             }
             catch (Exception)
             {
@@ -119,7 +228,33 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return "101010"; // Placeholder
+                //Using prebuilt function
+                // string binary = Convert.ToString(decimalNumber, 2);
+
+                //Manual build function
+                int l = Convert.ToInt32(Math.Log2(Math.Abs(decimalNumber))) + 1; //calculate the length of binary number
+                int[] digit = new int[l];
+                int remain = Math.Abs(decimalNumber);
+                string binary = "";
+                for (int i = l-1; i >= 0 ; i--){
+                    digit[i] = remain % 2;
+                    remain = remain /2;
+                    binary = digit[i] + binary;
+                }
+                //Assume we working on 16-bit binary. 
+                //Handle negative number: 
+                if (decimalNumber < 0){
+                    binary = "";
+                    for(int i = 0; i < 16 ; i ++){
+                        if(l - i > 0){
+                            binary = digit[i] + binary;
+                        }
+                        else{
+                            binary = "1" + binary;
+                        }
+                    }
+                }
+                return binary; 
             }
             catch (Exception)
             {
@@ -133,7 +268,18 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return 0; // Placeholder
+                if (nums.Length == 0){// Check the empty list
+                    return -99999;
+                }
+                int min = nums[0]; // init the value with 1st element;
+                for(int i = 0; i < nums.Length; i++) 
+                {
+                    if(nums[i] < min){ //found the smaller one, as rotated sort, this is the smallest too. We can exit here.
+                        min = nums[i];
+                        break;
+                    }
+                }
+                return min; // Placeholder
             }
             catch (Exception)
             {
@@ -147,7 +293,19 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return false; // Placeholder
+                string num = "";
+                string rev_num = "";
+                int digit = 0;
+                int remainder = Math.Abs(x); 
+                //Split digits from 
+                while(remainder >0){
+                    digit = remainder % 10;
+                    remainder = remainder / 10;
+                    num = digit + num;          //build string of right order number
+                    rev_num = rev_num + digit;  //build string of reversed order number
+                }
+                if (num == rev_num) return true;
+                else return false; 
             }
             catch (Exception)
             {
@@ -161,7 +319,24 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return 0; // Placeholder
+                //Out of range
+                int fn = 0;
+                if (n < 0 || n > 30){
+                    return -1; // assume that we return -1 for out of range 
+                }
+                if(n == 0) return 0;
+                else if(n ==1) return 1; 
+                else{ //for n > 1
+                    int fn_2 = 0;  //init f(n-2)
+                    int fn_1 = 1;  //init f(n-1)
+                    for (int i = 2; i <=n ; i++){ //calculate f(n)
+                        fn = fn_2 + fn_1; //calculate F(n)
+                        // Update f_2, f_1 for next cycle
+                        fn_2 = fn_1;
+                        fn_1 = fn;
+                    }
+                }
+                return fn; 
             }
             catch (Exception)
             {
